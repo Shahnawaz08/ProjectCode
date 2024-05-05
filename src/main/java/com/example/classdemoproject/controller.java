10645824cdeaaf5958a170be6cd2dae6c1a5ebf2
S M Shahnawaz <smshahnawaz8122000@gmail.com>
2024-05-05 18:54:38 +0530

    Added code for 2nd class

--- src/main/java/com/example/classdemoproject/controller.java
+++ src/main/java/com/example/classdemoproject/controller.java
@@ -1,9 +1,6 @@
 package com.example.classdemoproject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -13,8 +10,20 @@ public class controller {
 
   //  @GetMapping("/hello")
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello() {
+        return "Hello get!";
+    }
+    @PostMapping("/hello")
+    public String sayHelloPost() {
+        return "Hello post!";
+    }
+    @PatchMapping("/hello")
+    public String sayHelloPatch() {
+        return "Hello!";
+    }
+    @DeleteMapping("/hello")
+    public String sayHelloDelete() {
         return "Hello!";
     }
     @RequestMapping("/bye/{name}")
